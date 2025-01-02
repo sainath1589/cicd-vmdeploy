4fdc62d563e3929df22ff5f1320fd4453bd24588
sainath1589 <132442565+sainath1589@users.noreply.github.com>
2025-01-02 19:21:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "google" {
   project = "future-lane-444809-c3" # Replace with your GCP project ID
   region  = "us-central1"         # Replace with your preferred region
   zone    = "us-central1-a"       # Replace with your preferred zone
+  credentials = file("key-json") 
   
 }
 resource "google_compute_instance" "example" {
