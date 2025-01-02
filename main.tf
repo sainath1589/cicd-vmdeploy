b081ebf72474057275a07dec20a4b247b300bde5
Sainath Reddy <sainathreddy250@gmail.com>
2025-01-02 19:19:15 +0530

    fivecommit

--- main.tf
+++ main.tf
@@ -1,2 +1,27 @@
-hello friends
-ela unaru
+# Specify the provider
+provider "google" {
+  project = "future-lane-444809-c3" # Replace with your GCP project ID
+  region  = "us-central1"         # Replace with your preferred region
+  zone    = "us-central1-a"       # Replace with your preferred zone
+  
+}
+resource "google_compute_instance" "example" {
+  name         = "example-vm"
+  machine_type = "e2-micro" # Change as per your requirement
+  zone         = "us-central1-a" # Replace with your preferred zone
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts" # Debian 11 image
+    }
+  }
+
+  # Use the default network
+  network_interface {
+    network = "default" # Reference the default network
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+}
